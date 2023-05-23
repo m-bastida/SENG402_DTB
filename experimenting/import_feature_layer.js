0cd5bb4cb4fdca99acd1bab99f8cc1ef3d41c221
Max Bastida <ieb15@uclive.ac.nz>
2023-05-24 00:47:50 +1200

    Updated tree feature layer: Uses model from ArcGIS Tree height set according to data Trees appear at ground level Trees are green

--- experimenting/import_feature_layer.js
+++ experimenting/import_feature_layer.js
@@ -2,12 +2,13 @@ require([
   "esri/Map",
   "esri/views/SceneView",
   "esri/layers/FeatureLayer",
-], (Map, SceneView, FeatureLayer) => {
+  "esri/symbols/WebStyleSymbol"
+], (Map, SceneView, FeatureLayer, WebStyleSymbol) => {
   
 const view = new SceneView({
   container: "viewDiv",
   map: new Map({
-    basemap: "hybrid",
+    basemap: "osm",
     ground: "world-elevation",
 }),
 
@@ -21,9 +22,11 @@ const view = new SceneView({
   },
 });
 
+
+
   //  Creates a client-side FeatureLayer from an array of graphics
   function createLayer (){
-    return new FeatureLayer({
+    var layer =  new FeatureLayer({
       //4bc8537233344d04b9c37041f7204049
       portalItem: {
         id: "4bc8537233344d04b9c37041f7204049"
@@ -34,12 +37,48 @@ const view = new SceneView({
         name: "OBJECTID",
         type: "oid"
       }
-    ]
+    ],
+    renderer : {
+      type: "simple", // autocasts as new SimpleRenderer()
+      // symbol: {
+      //   type: "web-style", // autocasts as new WebStyleSymbol()
+      //   styleName: "esriRealisticTreesStyle",
+      //   name: "Other",
+      // },
+      visualVariables: [
+        {
+          type: "size",
+          axis: "height",
+          field: "Tree_H", // tree height
+          valueUnit: "meters"
+        },
+      ]
+    },
+    elevationInfo: {
+      mode: "on-the-ground",
+      offset: 0,
+    }
     });
+
+    var symbol = new WebStyleSymbol({
+      styleName: "esriRealisticTreesStyle",
+      name: "Other",
+    });
+
+    symbol.fetchSymbol()
+      .then(function(treeSymbol){
+        var objectSymbolLayer = treeSymbol.symbolLayers.getItemAt(0);
+        objectSymbolLayer.material = { color: "green" };
+        var renderer = layer.renderer.clone();
+        renderer.symbol = treeSymbol;
+        layer.renderer = renderer;
+      });
+      return layer
   }
 
   // Adds a given layer to the map in the view
   function addToView(layer){
+    console.log(layer)
     view.map.add(layer);
   }
 
